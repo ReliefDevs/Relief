aad932b5b37fe3b408ffab528f8c4142e56b0b68
Juan Marcos Lorenzo <jmlorenzo1993u@gmail.com>
2024-04-29 22:45:53 -0300

    services estructura y estilos basicos

--- src/components/servicios/Services.jsx
+++ src/components/servicios/Services.jsx
@@ -12,9 +12,9 @@ const Services = () => {
         <section className="services">
             <article className="row">
                 <h2>Servicios</h2>
-                <div className="services-web-development">
+                <div className="services-web-development d-flex justify-content-evenly align-item-center w-100">
                     <div>
-                        <h3>Web Development</h3>
+                        <h3 className="services-web-development-h3">Web Development</h3>
                         <p>¿Necesitas una web que te haga brillar?</p>
                         <p>¡Desarrolla tu proyecto online con nuestro equipo experto!</p>
                         <ul>
@@ -24,19 +24,54 @@ const Services = () => {
                             <li>Soporte técnico y mantenimiento continuo</li>
                         </ul>
                     </div>
-                    <div>
-
+                    <div className="services-web-development-image">
+                        
                     </div>
                 </div>
             </article>
-    <article className="row">
 
-    </article>
+            <article className="row">
+                <div className="services-ux-ui d-flex justify-content-evenly align-item-center w-100">
+                    <div>
+                        <h3 className="services-ux-ui-h3">UX/UI design</h3>
+                        <p>¿Necesitas una web que te haga brillar?</p>
+                        <p>¡Desarrolla tu proyecto online con nuestro equipo experto!</p>
+                        <ul>
+                            <li>Diseño web profesional y personalizado</li>
+                            <li>Soluciones a medida para tus necesidades</li>
+                            <li>Posicionamiento SEO para que te encuentren en Google</li>
+                            <li>Soporte técnico y mantenimiento continuo</li>
+                        </ul>
+                    </div>
+                    <div className="services-ux-ui-image">
+                        
+                    </div>
+                    <div>
+                </div>
+                </div>
+            </article>
 
-    <article className="row">
-
-    </article>
-</section>
+            <article className="row">
+                <div className="services-staff-on-demand d-flex justify-content-evenly align-item-center w-100">
+                    <div>
+                        <h3 className="services-staff-on-demand-h3">Staff on demand</h3>
+                        <p>¿Necesitas una web que te haga brillar?</p>
+                        <p>¡Desarrolla tu proyecto online con nuestro equipo experto!</p>
+                        <ul>
+                            <li>Diseño web profesional y personalizado</li>
+                            <li>Soluciones a medida para tus necesidades</li>
+                            <li>Posicionamiento SEO para que te encuentren en Google</li>
+                            <li>Soporte técnico y mantenimiento continuo</li>
+                        </ul>
+                    </div>
+                    <div className="services-staff-on-demand-image">
+                        
+                    </div>
+                    <div>
+                    </div>
+                </div>
+            </article>
+        </section>
     )
 }
 
