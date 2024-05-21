2db62d2f404f4306961b7bae98faa37855d14044
alan <relief.workplace@gmail.com>
2024-05-20 22:00:34 -0300

    Added json structure and img background, developed aboutus css and html,

--- src/components/aboutUs/AboutUs.jsx
+++ src/components/aboutUs/AboutUs.jsx
@@ -1,19 +1,25 @@
 
 import React from 'react';
+import "./aboutUs.css" 
 import { useLanguage } from '../../language';
 import "../../language";
 
 const AboutUs =() =>{
 
-    const { language, resources} = useLanguage();
-    const {aboutUs, aboutUsText,aboutUsPhrase,aboutUsPhraseBold} = resources[language];
+    const {language, resources} = useLanguage();
+    const {aboutUs} = resources[language];
 
     return(
-        <section className="about-us" >
+        <section className="about-us pt-4" >
                 <div className="col-12 p-0">
-                    <h1>{aboutUs.title}</h1>
-                    <p>{aboutUs.text}</p>
-                    <p>{aboutUs.phrase} <span>{aboutUs.phraseBold}</span></p>
+                    <h4 className='about-us-title'>{aboutUs.title}</h4>
+                    <div className="col-11 pb-4"><p className='about-us-text'>{aboutUs.text}</p></div>
+                    <div className="col-11">
+                        <p className='about-us-phrase'>"{aboutUs.phrase}</p>
+                    </div>
+                    <div className="col-9 d-flex justify-content-end">
+                        <p className='single'>en <span className='about-us-bold '>{aboutUs.phraseBold}"</span></p>
+                    </div>
                 </div>
         </section>
     )
