eda87fb5fa54cf011efaedba4a34ad08c519d374
Relief <relief.workplace@gmail.com>
2024-05-31 15:42:06 -0300

    fixed processes right white line, finished front contact us page

--- src/components/contactUs/ContactUs.jsx
+++ src/components/contactUs/ContactUs.jsx
@@ -4,24 +4,54 @@ import "./contactUs.css"
 import { useLanguage } from '../../language';
 import "../../language";
 import '@material/web/textfield/filled-text-field.js';
+import '@material/web/button/filled-button'
+
+/* import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser'; */
 
 
 const ContactUs =() =>{
 
     const {language, resources} = useLanguage();
     const {contactUs} = resources[language];
+    //const form = useRef();
+
+/*     const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+        .sendForm(
+            'service_c6fs9wo', 
+            'template_y9j1u5n', 
+            form.current, {
+            publicKey: 'uFf7_zN6AGSiJap4v',
+        })
+        .then(
+            () => {
+            console.log('SUCCESS!');
+            },
+            (error) => {
+            console.log('FAILED...', error.text);
+            },
+        );
+    }; */
+
+
+
 
     return(
         <section className="contact-us" >
                 <div className="col-12">
                     <h1 className='contact-title '>{contactUs.title}</h1>
                 </div>
-                <div className="">
-                    <form action="" className='d-flex flex-column'>
+                <div className="row ">
+                <div className="col-6">
+                    <form action=""  className='d-flex flex-column'>
 
                         <md-filled-text-field 
                             type="text"
                             id="full-name"
+                            name="user_name"
                             label={contactUs.fullName} 
                             supporting-text={contactUs.fullNameSupportText} >
                         </md-filled-text-field>
@@ -29,6 +59,7 @@ const ContactUs =() =>{
                         <md-filled-text-field 
                             type="email" 
                             id="mail" 
+                            name="user_email"
                             label={contactUs.mail} 
                             supporting-text={contactUs.mailSupportText}>
                         </md-filled-text-field>
@@ -36,12 +67,25 @@ const ContactUs =() =>{
                         <md-filled-text-field 
                             type="textarea" 
                             id="input-text" 
+                            name="message"
                             rows="5" 
                             cols="33" 
                             label={contactUs.text} 
                             supporting-text={contactUs.textSupportText}>
                         </md-filled-text-field>
-                    </form>
+                        <md-filled-button 
+                        id="complete-button"
+                        type="submit"
+                        value={contactUs.sendButton}
+                        ></md-filled-button>       
+                        </form>
+                </div>
+                <div className="col-6 d-flex justify-content-center">
+                    <img 
+                        src={require('./img/g10.png')} 
+                        className='img-fluid contact-img'
+                        alt=""/>
+                </div>
                 </div>
         </section>
     )
--- src/components/processes/Processes.jsx
+++ src/components/processes/Processes.jsx
@@ -14,7 +14,7 @@ const Processes = ()=>{
         <h2 className="text-center text-md-start">{processes.title}</h2>
     <div className="container-processes">
     
-        <div className="row">
+        <div className="row" id='processes-row'>
             <div className="col-sm-12 justify-content-center col-md-7 d-flex col-lg-6">  
                 <div className='subtitle-box p-5 col-10 justify-content-center'>
                     <div className="subtitle" onClick={() =>showDefinition(0)}>1.{processes.sub_title.analysis}</div>
@@ -25,8 +25,8 @@ const Processes = ()=>{
                     <div className="subtitle" onClick={() =>showDefinition(5)}>6.{processes.sub_title.repeat}</div>     
                 </div>
             </div>
-            <div className="col-sm-12 col-md-5 d-flex justify-content-center align-items-center col-lg-6 definition-container">   
-                <div className='definition-box col-12 d-flex justify-content-center align-items-center '>
+            <div className="col-sm-12 col-md-5  justify-content-center align-items-center col-lg-6 definition-container">   
+                <div className='definition-box justify-content-center align-items-center '>
                     <div className="definition"><p>{processes.defition.analysis}</p></div>
                     <div className="definition"><p>{processes.defition.design}</p></div>
                     <div className="definition"><p>{processes.defition.development}</p></div>
@@ -34,7 +34,7 @@ const Processes = ()=>{
                     <div className="definition"><p>{processes.defition.maintenance}</p></div>
                     <div className="definition"><p>{processes.defition.repeat}</p></div>
                 </div>
-            </div>
+            </div> 
         </div>
     </div>
 </section>
